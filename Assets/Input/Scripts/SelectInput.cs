ec54d358acde8fe1d08c29ca81f38ff5a935c925
Sokolov Nikita <sokolovnikita1240@gmail.com>
2023-10-18 15:03:10 +0300

    feat(mission-select-frame): add select frame

---
+++ Assets/Input/Scripts/SelectInput.cs
@@ -0,0 +1,69 @@
+using System.Reflection;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class SelectInput : MonoBehaviour
+{
+    [SerializeField] private Camera _camera;
+
+    private InputActions _inputActions;
+    private MissionBase _mission;
+
+    private void Awake()
+    {
+        _inputActions = new InputActions();
+    }  
+
+    private void OnEnable()
+    {
+        _inputActions.Enable();
+        _inputActions.Gameplay.Select.performed += Select;
+        _inputActions.Gameplay.Hover.performed += Hover;
+    }
+
+    private void OnDisable()
+    {
+        _inputActions.Disable();
+        _inputActions.Gameplay.Select.performed -= Select;
+        _inputActions.Gameplay.Hover.performed -= Hover;
+    }
+
+    private void Select(InputAction.CallbackContext obj)
+    {
+        Ray ray = DrawRay();
+        RaycastHit hit;
+        if (Physics.Raycast(ray, out hit))
+            hit.transform.gameObject.GetComponent<MissionBase>()?.Select();    
+    }
+
+    private void Hover(InputAction.CallbackContext obj)
+    {
+        Ray ray = DrawRay();
+        RaycastHit hit;
+        if (Physics.Raycast(ray, out hit))
+        {
+            _mission = hit.transform?.gameObject.GetComponent<MissionBase>();
+            _mission?.EnableFrame();
+        }
+        else
+        {
+            _mission?.DisableFrame();
+        }
+        
+    }
+
+    private Ray DrawRay()
+    {
+        Vector2 clickPosition = new Vector2(_inputActions.Gameplay.Hover.ReadValue<Vector2>().x,
+            _inputActions.Gameplay.Hover.ReadValue<Vector2>().y);
+        Vector3 viewPosition = new Vector3(clickPosition.x, clickPosition.y, -1 *
+            _camera.transform.position.z);
+
+        Vector3 rayStart = _camera.ScreenToWorldPoint(viewPosition);
+        Vector3 rayEnd = new Vector3(rayStart.x, rayStart.y, -200);
+
+        Ray ray = new Ray(rayStart, rayEnd);
+
+        return ray;
+    }
+}
--- Assets/Mission/Scripts/Abstractions/MissionBase.cs
+++ Assets/Mission/Scripts/Abstractions/MissionBase.cs
@@ -3,21 +3,42 @@ using UnityEngine;
 public abstract class MissionBase : MonoBehaviour
 {
     [SerializeField] protected IChooseable _chooseStrategy;
+    [SerializeField] protected GameObject _hoverFrame;
+
+    private bool _isHovered = false;
+   
 
     private void Start()
     {
         InitStrategies();
     }
 
-    private void Update()
+    public bool IsHovered
     {
-        if (Input.GetKeyDown(KeyCode.Space))
-            Choose();
+        get { return _isHovered; }
+        set 
+        { 
+            _isHovered = value;
+            if (_isHovered == true)
+                EnableFrame();
+            if (_isHovered == false)
+                DisableFrame();
+        }
     }
 
-    public void Choose()
+    public void Select()
     {
         _chooseStrategy.Choose();
+    }
+
+    public void EnableFrame()
+    {
+        _hoverFrame.SetActive(true);
+    }
+
+    public void DisableFrame()
+    {
+        _hoverFrame.SetActive(false);
     }
 
     protected abstract void InitStrategies();
